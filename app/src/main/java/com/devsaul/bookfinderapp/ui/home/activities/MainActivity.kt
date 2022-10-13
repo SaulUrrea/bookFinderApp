f39ccf620299586445f790372c1a8178fb1c6859
Saul Esteban Urrea Osorio <saul5577@gmail.com>
2022-10-13 08:54:29 -0500

    feat: se agrega el nav graph y los fragmentos principales

--- app/src/main/java/com/devsaul/bookfinderapp/ui/home/activities/MainActivity.kt
+++ app/src/main/java/com/devsaul/bookfinderapp/ui/home/activities/MainActivity.kt
@@ -2,14 +2,32 @@ package com.devsaul.bookfinderapp.ui.home.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.devsaul.bookfinderapp.R
+import com.devsaul.bookfinderapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpBottomNav()
+    }
+
+    private fun setUpBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
